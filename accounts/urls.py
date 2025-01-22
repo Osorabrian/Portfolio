0fbb9ec443b023123c8a4384f4361887f1db1fc4
Osorabrian <osorabrian@gmail.com>
2025-01-21 16:08:03 -0800

    format to serve media file

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,15 @@
 from django.urls  import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('', views.home, name="home")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
