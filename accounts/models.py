546fb9548463af6c888fcb9def18278fc69bb881
Osorabrian <osorabrian@gmail.com>
2025-01-23 09:45:21 -0800

    create message model and model form

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-
+class Message(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    message=models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.name} has written a message"
+    
+    class Meta:
+        ordering = ['-published']
+        indexes = [
+            models.Index(fields=['-published'])
+            ]
