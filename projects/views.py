a4e31a2fedb769523b7ef1af2dd6d620b32ecdb3
Osorabrian <osorabrian@gmail.com>
2025-01-24 09:14:41 -0800

    edit css files

--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,7 @@ from .models import Project
 
 # Create your views here.
 def projects_list(request):
-    projects = Project.objects.all()
+    projects = Project.objects.all().order_by('published')
     return render(
         request,
         'projects/projects.html',
