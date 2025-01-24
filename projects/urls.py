5184cd4a08cf3cfdc6c6b9a826d5a5a67ef9a157
Osorabrian <osorabrian@gmail.com>
2025-01-24 10:10:27 -0800

    add search functionality

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 namespace = "projects"
 urlpatterns = [
-    path("", views.projects_list, name="projects")
+    path("", views.projects_list, name="projects"),
+    path("<str:query>", views.projects_list, name="projects_search")
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from .models import Project
+from .forms import SearchForm
 
 # Create your views here.
 def projects_list(request):
-    projects = Project.objects.all().order_by('published')
+    
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data['search']
+            projects = Project.objects.filter(title__icontains=query)
+            
+    else:
+        form = SearchForm()
+        projects = Project.objects.all().order_by('published')
+        
     return render(
         request,
         'projects/projects.html',
-        {'projects':projects}
+        {'projects':projects, 'form':form}
     )
