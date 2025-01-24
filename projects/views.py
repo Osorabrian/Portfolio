4ec56cb1c354adbcca84d63898e7dd880927269c
Osorabrian <osorabrian@gmail.com>
2025-01-24 11:47:50 -0800

    format static files

--- projects/views.py
+++ projects/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 from .models import Project
 from .forms import SearchForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 def projects_list(request):
     
+    query = None
     if request.method == "POST":
         form = SearchForm(request.POST)
         if form.is_valid():
             query = form.cleaned_data['search']
-            projects = Project.objects.filter(title__icontains=query)
-            
     else:
         form = SearchForm()
+
+    if query:
+        projects = Project.objects.filter(title__icontains=query).order_by('published') 
+    else:
         projects = Project.objects.all().order_by('published')
+        
+    paginator = Paginator(projects, 2)
+    page_number = request.GET.get('page', 1)
+    projects = paginator.page(page_number)
         
     return render(
         request,
