6ae3dd7ebd5cd0b3e80b9394baba3af2afd9475d
Osorabrian <osorabrian@gmail.com>
2025-01-28 13:38:58 -0800

    add email validation in user registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,8 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'message']
-        
+ 
+User = get_user_model()       
 class UserRegistrationForm(forms.ModelForm):
     
     password = forms.CharField(widget=forms.PasswordInput)
@@ -21,5 +22,10 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("Passwords do not match")
         return cd['password2']
-   
-        
+    
+    def clean_email(self):
+        cd = self.cleaned_data
+        email = User.objects.filter(email=cd['email']).exists()
+        if email:
+            raise forms.ValidationError("Email already exists.")
+        return cd['email']
