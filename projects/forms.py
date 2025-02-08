9ad24e37617398644a90603f8efd081b0ab0663a
Osorabrian <osorabrian@gmail.com>
2025-02-08 12:22:41 -0800

    format search input and icon

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    search = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Search', 'type':'search'}))
+    search = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'Search for a Project', 'type':'search', 'class':'form-control','id':'search-input'}))
