c3f97288ab05fc5bb1eabcdb526fe9b85acef694
Osorabrian <osorabrian@gmail.com>
2025-02-11 23:20:53 -0800

    create both experince and education views

--- about/views.py
+++ about/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Experience, Education
 
 # Create your views here.
+def education_view(request):
+    education = Education.objects.all()
+    return render(
+        request,
+        'about/about.html',
+        {'education':education}
+    )
+    
+def experience_view(request):
+    experience = Experience.objects.all()
+    return render(
+        request,
+        'about/about.html',
+        {'experience':experience}
+    )
