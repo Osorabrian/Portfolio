c4e5a0a47b73745fd3a0f2272d72617804cac591
Osorabrian <osorabrian@gmail.com>
2025-02-11 23:03:16 -0800

    create admin for education and add blank and true properties in end dates for both education and experience model

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Education, Experience
 
 # Register your models here.
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ['school','course','start_date','end_date']
+    list_filter = ['school']
+    date_hierachy = 'start_date'
+    search_fields = ['school','course']
+    facets = show.Facets.ALWAYS
+    
--- about/models.py
+++ about/models.py
@@ -5,7 +5,7 @@ class Education(models.Model):
     school=models.CharField(max_length=250)
     course=models.CharField(max_length=250)
     start_date=models.DateField()
-    end_date=models.DateField()
+    end_date=models.DateField(blank=True, null=True)
     description=models.TextField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -23,7 +23,7 @@ class Experience(models.Model):
     position=models.CharField(max_length=250)
     company=models.CharField(max_length=250)
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
     achievement = models.TextField()
     description = models.TextField()
     
