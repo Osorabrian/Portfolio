15c08e3f8b3eea2927d2dcb464b1efb6fb6dc6a0
Osorabrian <osorabrian@gmail.com>
2025-02-23 11:44:25 -0800

    format search bar input

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    search = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'Search for a Project', 'type':'search', 'class':'form-control','id':'search-input'}))
+    search = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'Search for a Project', 'type':'search', 'class':'form-control rounded-0','id':'search-input'}))
