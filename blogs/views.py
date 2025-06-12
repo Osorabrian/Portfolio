256529df2769b32e541628fb19ea441acb71bd75
Osorabrian <osorabrian@gmail.com>
2025-06-11 19:52:02 -0700

    create post list template

--- blogs/views.py
+++ blogs/views.py
@@ -4,16 +4,17 @@ from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(status=Post.Status.PUBLISHED)
+    posts = Post.objects.all()
     return render(
         request,
         'blogs/list.html',
-        {posts: posts})
+        {'posts': posts}
+    )
     
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, status=Post.Status.PUBLISHED)
     return render(
         request,
         'blogs/detail.html',
-        {post:post}
+        {'post':post}
     )
