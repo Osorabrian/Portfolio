3243e10ce5882675dc78b19119273977b1f2e00e
Osorabrian <osorabrian@gmail.com>
2025-06-12 08:42:31 -0700

    align the pagination component to the center

--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .models import Post
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(status=Post.Status.PUBLISHED)
+    paginator = Paginator(posts, 1)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(
         request,
         'blogs/list.html',
