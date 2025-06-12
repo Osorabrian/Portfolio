26bf73b2c77b79db44b34a500e461c5cd6193f30
Osorabrian <osorabrian@gmail.com>
2025-06-12 11:05:39 -0700

    create education model

--- about/models.py
+++ about/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Edcation(models.Model):
+    school = models.CharField(max_length=250)
+    course = models.CharField(max_length=250)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    description = models.TextField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.course} taken at {self.school} added."
+    
+    class Meta:
+        ordering = ['-start_date']
+        indexes = [
+            models.Index(fields=['-start_date']),
+        ]
+        
+class Experience(models.Model):
+    company = models.CharField(max_length=250)
+    position = models.CharField(max_length=250)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    description = models.TextField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.position} at {self.company} added."
+    
+    class Meta:
+        ordering = ['-start_date']
+        indexes = [
+            models.Index(fields=['-start_date']),
+        ]
