be6ea7cd77d7ad2d941bd2c9eac7d639edaac23a
Osorabrian <osorabrian@gmail.com>
2025-06-16 20:55:49 -0700

    format search button

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,11 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    search = forms.CharField(label="", widget=forms.TextInput(attrs={'placeholder':'Search for a Project', 'type':'search', 'class':'form-control rounded-0','id':'search-input'}))
+    search = forms.CharField(label="", required=False, widget=forms.TextInput(attrs={
+        'placeholder':'Search for a Project', 
+        'type':'search', 
+        'class':'form-control rounded-0',
+        'id':'search-input', 
+        'style':'width: 300px; height: 40px; border-radius: 10%; border: 1px solid #00ffff; background-color: #ffffff; font-size: 16px; font-weight: 500; padding: 10px; margin-right: 10px;'}))
+    
+    
